34042871472672aaaa56e876d4199b283b644153
Gantcho Kojuharov <gantcho@greyarch.net>
2018-06-03 13:47:24 +0200

    put entrypoint args in double quotes to avoid args with spaces problems

--- hyperdev.sh
+++ hyperdev.sh
@@ -2,4 +2,4 @@
 
 envsubst < ${GOGS_CUSTOM}/conf/app.ini > ${GOGS_CUSTOM}/conf/app.ini
 envsubst < ${GOGS_CUSTOM}/conf/auth.d/ldap_bind_dn.conf > ${GOGS_CUSTOM}/conf/auth.d/ldap_bind_dn.conf
-/app/gogs/docker/start.sh $@
+/app/gogs/docker/start.sh "$@"
